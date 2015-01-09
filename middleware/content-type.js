567318151ab12e045f4a51fa79b21dde91daaeb3
Ben Parnell <benjaminparnell.94@gmail.com>
2015-01-09 12:29:32 +0000

    allow multiple content-type headers
    currently fixed to 'application/json' and 'application/csv'

---
+++ middleware/content-type.js
@@ -0,0 +1,37 @@
+module.exports = contentTypeMiddleware
+
+function contentTypeMiddleware(types) {
+
+  /*
+  * Middleware to ensure the API only handles request
+  * payloads when the content type is declared to be valid.
+  */
+  function middleware(req, res, next) {
+    if ([ 'POST', 'PUT', 'PATCH' ].indexOf(req.method) === -1) return next()
+    if (isValidType(req.headers['content-type'])) return next()
+    res.status(400).json({ error: 'API does not support ' + req.headers['content-type'] + ' content-type' })
+  }
+
+  /*
+  * Check if the body of a header indicates that the request body of a type
+  * that we have deemed valid
+  */
+  function isValidType(contentTypeHeader) {
+    if (!contentTypeHeader) return false
+
+    // This header is allowed two components: type and charset, eg:
+    //   'Content-Type: application/json; charset=utf-8'
+    // This check is only concerning the type component so
+    // split on the ';' and trim any whitespace before comparing.
+    var contentType = contentTypeHeader.split(';')[0].trim()
+
+    if (types.indexOf(contentType) === -1) {
+      return false
+    }
+
+    return true
+  }
+
+  return middleware
+
+}
