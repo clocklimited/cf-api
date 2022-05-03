d1e3e0c7c40987c069ea7433c533f0c4708f711a
Jack Burgess <jackburgess124@gmail.com>
2022-05-03 20:12:58 +0100

    fix(server): i use this

--- src/lib/server.ts
+++ src/lib/server.ts
@@ -1,6 +1,6 @@
 import bodyParser from 'body-parser'
 import express from 'express'
-// import responseTime from 'response-time'
+import responseTime from 'response-time'
 
 import { CfApiOptions, OriginChecker } from './api'
 import accepts from './middleware/accepts'
